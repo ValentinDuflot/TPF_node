2713760fed62c8601c5e125c0eec2f8859f11e52
Valentin Duflot <duflot.valentin@gmail.com>
2022-12-16 10:21:03 +0100

    <Chores> initialisation Redux pour l'authentification

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from './components/Navbar'
 import Login from './components/Login';
 import Register from './components/Register';
 
-//import Connect from './connect.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
-    <div className="container-fluid nav-justified" >
-      <div className="tab-content">
-        <Login />
-        <Register />
+    <Provider store={store}>
+      <div className="container-fluid nav-justified" >
+        <div className="tab-content">
+          <Login />
+          <Register />
+        </div>
       </div>
-    </div>
+    </Provider>
   </React.StrictMode>
 );
